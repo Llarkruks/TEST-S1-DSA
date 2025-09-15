8acedb4ac9236158a1bb665f91f222e77dc28d88
Llarkruks <arnau.cepero@estudiantat.upc.edu>
2025-09-15 14:24:00 +0200

    Version Funcional

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,35 @@
+import java.util.Arrays;
+public class Main {
+    public static double suma(Figura[] v) {
+        double ret = 0;
+        for (Figura f : v) {
+            ret += f.area();
+        }
+        return ret;
+    }
+    public static void sort(Figura[] v){
+        Arrays.sort(v);
+    }
+    public static void print(Figura[] v) {
+        for (Figura figura : v) {
+            System.out.println("La figura es: "+figura.toString()+" i te "+figura.area()+" m^2");
+        }
+    }
+    public static void main(String[] args) {
+        Figura[] v = new Figura[4];
 
-public static void main(String[] args) {
-    Shape[] v = new Shape[4];
+        v[0] = new Rectangle(5,3);
+        v[1] = new Cercle(5);
+        v[2] = new Quadrat(5, 5);
+        v[3] = new Triangle(5,9);
 
-    v[0] = new Rectangle(5, 3);
-    v[1] = new Circle(5);
-    v[2] = new Square(5);
-    v[3] = new Circle(120);
-k
-    double res = sum(v);
-j
-    System.out.println("sum: " + res);
+        double res = suma(v);
 
-    print(v);
+        System.out.println("Àrea total és: "+res + " m^2");
 
-    sort(v);
+        print(v);
 
-    print(v);
+        sort(v);
+
+    }
 }
